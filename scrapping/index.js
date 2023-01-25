8d9ee7fa6dbc24d64d5be097bb7f4494a1d44a34
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2023-01-25 16:30:11 +0100

    Scrape from params

--- scrapping/index.js
+++ scrapping/index.js
@@ -1,8 +1,21 @@
 import { startBrowser } from './browser.js'
-import { scrapeAll } from './page-controller.js'
+import { scrapeAll, scrapeLeaderBoard, scrapeMatches, scrapeTeams } from './page-controller.js'
+import { argv } from 'node:process'
+
+const scrapeParam = argv[2]
 
 //  Start the browser and create a browser instance
 const browserInstance = startBrowser()
 
 //  Pass the browser instance to the scraper controller
-scrapeAll(browserInstance)
+if (scrapeParam === 'all') {
+  scrapeAll(browserInstance)
+} else if (scrapeParam === 'leaderboard') {
+  scrapeLeaderBoard(browserInstance)
+} else if (scrapeParam === 'matches') {
+  scrapeMatches(browserInstance)
+} else if (scrapeParam === 'teams') {
+  scrapeTeams(browserInstance)
+} else {
+  console.log('Nothing to scrape')
+}
