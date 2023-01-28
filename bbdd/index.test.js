7c84108a3bf444d680fccc8f9ea918ea55eb423d
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2023-01-28 11:29:43 +0100

    Adding team colors test

--- bbdd/index.test.js
+++ bbdd/index.test.js
@@ -1,14 +1,15 @@
 import { describe, expect, it } from 'vitest'
-import { writeDBFile, TEAMS, MATCHES, getImageFromTeam, LEADERBOARD } from './index'
+import { writeDBFile, TEAMS, MATCHES, getImageFromTeam, LEADERBOARD, TEAM_COLORS } from './index'
 
 describe('testing bbdd functionality', () => {
 	it('saves data to JSON file', () => {
 		writeDBFile('dummy', { data: 'dummy' })
 	})
-	it('teams matches leaderboard have values', () => {
+	it('teams matches leaderboard and colors have values', () => {
 		expect(TEAMS).toBeDefined()
 		expect(MATCHES).toBeDefined()
 		expect(LEADERBOARD).toBeDefined()
+		expect(TEAM_COLORS).toBeDefined()
 	})
 	it('returns team image', () => {
 		const image = getImageFromTeam({ name: 'Oxygen Valiants' })
