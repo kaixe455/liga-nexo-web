47e8d595cfdd2dadb4bb450e2ae60c720b69b968
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2023-02-06 15:17:52 +0100

    Create sort function in utils file

---
+++ utils/utils.js
@@ -0,0 +1,7 @@
+export function sortByProperty (property) {
+	return function (a, b) {
+		if (a[property] > b[property]) { return 1 } else if (a[property] < b[property]) { return -1 }
+
+		return 0
+	}
+}
